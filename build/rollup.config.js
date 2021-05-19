624c5f1e75bddc11ef3d387789de086db7abd692
wmc <wu__mengcheng@163.com>
2021-05-19 23:18:41 +0800

    wmc

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,6 +2,7 @@ import vue from 'rollup-plugin-vue'
 import css from 'rollup-plugin-css-only'
 import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+//插件可以告诉 Rollup 如何查找外部模块
 import { name } from '../package.json'
 const file = type => `dist/${name}.${type}.js`
 const overrides = {
